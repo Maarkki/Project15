e6ecec622f4c4eb8d626a77904713747a046fc89
Marki <ser45393@gmail.com>
2024-10-28 15:35:08 +0700

    pugV01

--- front/src/lib/translators/username.translator.js
+++
@@ -1,5 +0,0 @@
-export default function usernameTranslator(name, soname, username) {
-  if ((name === null && soname === null) || (name == "" && soname == ""))
-    return username;
-  return soname + " " + name;
-}
